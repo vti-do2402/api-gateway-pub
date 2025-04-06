c3c0e6277ce30eb4ee8fce93e4e2f8f7af087ebf
Quentin <s3981278@rmit.edu.vn>
2025-04-06 23:04:50 +0700

    fix: Add github workflows and fix tests

--- src/test/java/com/example/api_gateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/example/api_gateway/ApiGatewayApplicationTests.java
@@ -2,8 +2,13 @@ package com.example.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.cloud.config.enabled=false",
+    "spring.cloud.discovery.enabled=false"
+})
 class ApiGatewayApplicationTests {
 
 	@Test
